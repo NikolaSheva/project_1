d4a1d90b985634dc9d1dd3edc4b51fc4c0763918
NilolaSheva <niksheva7@gmail.com>
2025-05-19 01:26:55 +0500

    change gunicorn.conf

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,7 +3,12 @@ import multiprocessing
 bind = "0.0.0.0:10000"
 workers = multiprocessing.cpu_count() * 2 + 1
 threads = 2
-timeout = 120  # Увеличьте таймаут для Render
+timeout = 120
 keepalive = 5
 max_requests = 500
 max_requests_jitter = 50
+
+# (Необязательно, но полезно для отладки)
+accesslog = "-"
+errorlog = "-"
+loglevel = "info"
