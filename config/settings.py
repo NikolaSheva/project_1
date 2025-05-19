1a3b77bff4ac153ffd75329d20750f04d3a6a19d
NilolaSheva <niksheva7@gmail.com>
2025-05-19 12:46:53 +0500

    changed load_initial_data.py

--- config/settings.py
+++ config/settings.py
@@ -27,13 +27,13 @@ TEMPLATE_DIR = BASE_DIR / 'templates'
 # SECRET_KEY = os.getenv('SECRET_KEY', 'dummy-key-for-dev-only')
 # DEBUG = os.getenv('DEBUG', 'False') == 'True'
 # ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
-
+print("DATABASE_URL:", os.getenv("DATABASE_URL"))
 # Security settings
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = env.bool("DEBUG", default=False)
 # Для ALLOWED_HOSTS
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', '127.0.0.1'])
-print("DATABASE_URL:", os.getenv("DATABASE_URL"))
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+
 
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
--- watch/management/commands/load_initial_data.py
+++ watch/management/commands/load_initial_data.py
@@ -4,6 +4,8 @@ from django.core.management.base import BaseCommand
 from django.core.management import call_command
 from django.conf import settings
 
+from watch.models import Product, Brand, City
+
 class Command(BaseCommand):
     help = 'Load initial data from fixtures if not already loaded.'
 
@@ -30,19 +32,24 @@ class Command(BaseCommand):
                 self.stdout.write("✔ Initial data already loaded (hash match). Skipping.")
                 return
 
-        self.stdout.write("🗂 Loading initial data...")
+        self.stdout.write("🧹 Очистка старых данных...")
+        Product.objects.all().delete()
+        Brand.objects.all().delete()
+        City.objects.all().delete()
+
+        self.stdout.write("🗂 Загрузка новых данных...")
         for fixture in fixtures:
             path = os.path.join(base_dir, fixture)
             if os.path.exists(path):
-                self.stdout.write(f"📦 Importing {fixture}...")
+                self.stdout.write(f"📦 Импорт: {fixture}")
                 call_command('loaddata', path)
             else:
-                self.stdout.write(f"⚠️ Fixture {fixture} not found, skipping.")
+                self.stdout.write(f"⚠️ Фикстура {fixture} не найдена. Пропуск.")
 
-        # Save new hash
+        # Сохраняем новый хеш
         with open(hash_file, 'w') as f:
             f.write(current_hash)
-        self.stdout.write("✅ Initial data loaded and hash updated.")
+        self.stdout.write("✅ Загрузка завершена, хеш обновлён.")
 
 # from django.core.management.base import BaseCommand
 # from django.core.management import call_command
